f324fd7ac28bed34505699d3a162bdb6f91aa059
Mohamed Essam <mohamed.essam.arafa@gmail.com>
2017-12-13 16:06:29 +0200

    Skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ResultsPageComponent } from './results-page/results-page.component';
+import { ResultsComponent } from './results/results.component';
+import { ResultComponent } from './result/result.component';
+import { ResultDetailsComponent } from './result-details/result-details.component';
+import { SearchComponent } from './search/search.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { SearchServiceService } from './search-service.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ResultsPageComponent,
+    ResultsComponent,
+    ResultComponent,
+    ResultDetailsComponent,
+    SearchComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [SearchServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
