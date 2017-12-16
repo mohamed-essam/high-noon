cab48c34ccdea759e6d7ef9d9a516a016f5d59a2
Mahmoud Abdou <mahmoud2051998@gmail.com>
2017-12-16 15:27:40 +0200

    Initial results page

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Result } from '../result'
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
-
+// @Input() results : Result []
+  results: Result[] = [
+    {
+      title: "any",
+      url: "anyyy.com",
+      body: "anybody"
+    },
+    {
+      title: "any",
+      url: "anyyy.com",
+      body: "anybody"
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
