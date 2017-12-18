26c14438de3341ae3a0037ced9a355d883b19ae6
Mohamed Essam <mohamed.essam.arafa@gmail.com>
2017-12-18 12:58:29 +0200

    Main page styling (#4)
    * Increase interactivity in main-page and re-style search
    
    * Add random mccree hints
    
    * Change background color to light grey

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -4,6 +4,15 @@ import { SearchService } from '../search.service'
 import { Result } from '../result'
 import { Observable } from 'rxjs/Observable'
 
+const randomHints = [
+  "It's high noon somewhere in the world",
+  "Watch and learn",
+  "Reach for the sky",
+  "You done?",
+  "That'll do fine",
+  "This life's never uneventful"
+]
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -17,9 +26,14 @@ export class SearchComponent implements OnInit {
     text: [""]
   });
 
-  constructor(private fb: FormBuilder, private searchService: SearchService) { }
+  randomHint: string;
+
+  constructor(private fb: FormBuilder, private searchService: SearchService) {
+    this.ngOnInit();
+  }
 
   ngOnInit() {
+    this.randomHint = randomHints[Math.floor(Math.random() * randomHints.length)];
   }
 
   search($event) {
